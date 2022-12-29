bc5412a469e5f94ae9907a3d2eb3062080f2bfe3
maxmiroslavski <maksimsaeros@gmail.com>
2022-12-29 12:00:55 +0500

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,15 @@ import App from './App';
 import { Provider } from 'react-redux/es/exports';
 import { store } from './store';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<Provider store={store}>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</Provider>
 );
